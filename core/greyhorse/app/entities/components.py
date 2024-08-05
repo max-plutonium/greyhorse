f18eccba02197dced32cf6baa356a0486594d054
Max Plutonium <plutonium.max@gmail.com>
2024-08-05 15:44:31 +0300

    [core] Refactoring of Component class

--- core/greyhorse/app/entities/components.py
+++ core/greyhorse/app/entities/components.py
@@ -1,12 +1,13 @@
+from greyhorse.app.abc.collectors import Collector, MutCollector
 from greyhorse.app.abc.controllers import Controller
 from greyhorse.app.abc.operators import Operator
 from greyhorse.app.abc.providers import Provider
+from greyhorse.app.abc.selectors import Selector, ListSelector
 from greyhorse.app.abc.services import Service, ServiceState
-from greyhorse.app.registries import MutResourceRegistry
+from greyhorse.app.registries import MutDictRegistry
 from greyhorse.app.schemas.component import ComponentConf
 from greyhorse.enum import Enum, Unit, Struct
-from greyhorse.maybe import Maybe, Nothing
-from greyhorse.result import Ok, Err
+from greyhorse.maybe import Maybe
 
 
 class ComponentState(Enum):
@@ -19,16 +20,16 @@ class ComponentState(Enum):
 class Component:
     def __init__(
         self, name: str, conf: ComponentConf, path: str,
-        controllers: list[Controller], services: list[tuple[str, Service]],
+        controllers: list[Controller], services: list[Service],
     ):
         self._name = name
         self._conf = conf
         self._path = path
-        self._controllers: dict[type[Controller], Controller] = {type(c): c for c in controllers}
-        self._service_names: dict[str, Service] = {k: v for (k, v) in services}
-        self._service_types: dict[type[Service], Service] = {type(v): v for (_, v) in services}
-        self._providers = MutResourceRegistry[Provider]()
-        self._operators = MutResourceRegistry[Operator]()
+
+        self._controllers: list[Controller] = controllers
+        self._services: dict[type[Service], Service] = {type(s): s for s in services}
+
+        self._operator_reg = MutDictRegistry[type, Operator]()
 
     @property
     def name(self) -> str:
@@ -38,48 +39,33 @@ class Component:
     def path(self) -> str:
         return self._path
 
-    def get_service(self, key: str | type) -> Maybe[Service]:
-        match key:
-            case str():
-                return Maybe(self._service_names.get(key))
-            case type():
-                return Maybe(self._service_types.get(key))
-        return Nothing
+    def get_service[T: Service](self, type_: type[T]) -> Maybe[T]:
+        return Maybe(self._services.get(type_))
 
-    def get_providers(self) -> list[tuple[type[Provider], Provider]]:
-        result = []
-
-        for resource_data in self._conf.resources:
-            for prov_type in resource_data.providers:
-                if provider := self._providers.get(class_=prov_type).unwrap_or_none():
-                    result.append((prov_type, provider))
-
-        return result
-
-    def get_operators(self) -> list[tuple[type[Operator], Operator]]:
-        result = []
-
-        for t in self._conf.operators:
-            if operator := self._operators.get(class_=t):
-                result.append((t, operator.unwrap()))
-
-        return result
-
-    def get_provider[P: Provider](self, class_: type[P]) -> Maybe[P]:
-        return self._providers.get(class_=class_)
-
-    def setup(self) -> ComponentState:
+    def setup(
+        self,
+        prov_selector: Selector[type[Provider], Provider],
+        prov_collector: Collector[type[Provider], Provider],
+        op_selector: ListSelector[type, Operator],
+    ) -> ComponentState:
         count = 0
         errors = []
 
-        for svc in self._service_types.values():
-            match svc.setup(self._providers, self._operators):
-                case Ok(state):
-                    if isinstance(state, ServiceState.Active):
-                        count += 1
+        for op_conf in self._conf.operators:
+            for _, operator in op_selector.items(lambda t: t == op_conf.resource):
+                self._operator_reg.add(op_conf.resource, operator)
 
-                case Err(e):
-                    errors.append(e.message)
+        for ctrl in self._controllers:
+            if ctrl.setup(prov_selector, self._operator_reg) \
+                    .map_err(lambda e: errors.append(e.message)) \
+                    .unwrap_or(False):
+                count += 1
+
+        for svc in self._services.values():
+            if svc.setup(self._operator_reg, prov_collector).map_err(
+                lambda e: errors.append(e.message)
+            ).map(lambda s: isinstance(s, ServiceState.Active)):
+                count += 1
 
         if not errors:
             return ComponentState.Ready
@@ -88,21 +74,33 @@ class Component:
         else:
             return ComponentState.Error(errors=errors)
 
-    def teardown(self) -> ComponentState:
+    def teardown(
+        self,
+        prov_selector: Selector[type[Provider], Provider],
+        prov_collector: MutCollector[type[Provider], Provider],
+        op_selector: ListSelector[type, Operator],
+    ) -> ComponentState:
         count = 0
         errors = []
 
-        for svc in reversed(self._service_types.values()):
-            match svc.teardown(self._providers, self._operators):
-                case Ok(state):
-                    if state == ServiceState.Idle:
-                        count += 1
+        for svc in reversed(self._services.values()):
+            if svc.teardown(self._operator_reg, prov_collector).map_err(
+                lambda e: errors.append(e.message)
+            ).map(lambda s: s == ServiceState.Idle):
+                count += 1
 
-                case Err(e):
-                    errors.append(e.message)
+        for ctrl in reversed(self._controllers):
+            if ctrl.teardown(prov_selector, self._operator_reg) \
+                    .map_err(lambda e: errors.append(e.message)) \
+                    .unwrap_or(False):
+                count += 1
+
+        for op_conf in reversed(self._conf.operators):
+            for _, operator in op_selector.items(lambda t: t == op_conf.resource):
+                self._operator_reg.remove(op_conf.resource, operator)
 
         if not errors:
-            return ComponentState.Idle
+            return ComponentState.Ready
         elif count > 0:
             return ComponentState.Partial(count=count, errors=errors)
         else:
