78c9cb8008a79dda48b193c81167ce0d5ee9eb4e
Max Plutonium <plutonium.max@gmail.com>
2024-09-20 14:43:52 +0300

    [core] json.dumps, json.dumps_raw

--- core/greyhorse/utils/json.py
+++ core/greyhorse/utils/json.py
@@ -1,15 +1,38 @@
-from typing import Any
+import decimal
+from typing import Any, Callable
 
 from orjson import orjson
 
 
-def dumps(content: Any, use_indent: bool = False, sort_keys: bool = False, **kwargs) -> bytes:
+def stringify(obj):
+    if isinstance(obj, decimal.Decimal):
+        return str(obj)
+    raise TypeError
+
+
+def dumps_raw(
+    content: Any,
+    use_indent: bool = False,
+    sort_keys: bool = False,
+    stringify_fn: Callable[[Any], str] | None = None,
+    **kwargs,
+) -> bytes:
     option = orjson.OPT_SERIALIZE_UUID
     if use_indent:
         option |= orjson.OPT_INDENT_2
     if sort_keys:
         option |= orjson.OPT_SORT_KEYS
-    return orjson.dumps(content, option=option, **kwargs)
+    return orjson.dumps(content, option=option, default=stringify_fn or stringify, **kwargs)
+
+
+def dumps(
+    content: Any,
+    use_indent: bool = False,
+    sort_keys: bool = False,
+    stringify_fn: Callable[[Any], str] | None = None,
+    **kwargs,
+) -> str:
+    return dumps_raw(content, use_indent, sort_keys, stringify_fn, **kwargs).decode('utf-8')
 
 
 def loads(data: bytes | bytearray | memoryview | str) -> Any:
